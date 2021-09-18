2802380493ae0c9c1e654af3ddb23049b9af53c9
Erikson TF <sonyharv9@gmail.com>
2021-09-17 22:57:21 -0700

    Add select, inputs, btn exportar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [linea1, setLinea1] = useState("Hola Mundo");
+  const [linea2, setLinea2] = useState("Bienvenidos!!!");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select>
+        <option value="fire">Casa en llamas</option>
+        <option value="futurama">Futurama</option>
+        <option value="history">History</option>
+        <option value="matrix">Matrix</option>
+        <option value="philosoraptor">Philosoraptor</option>
+        <option value="smart">Smart Guy</option>
+      </select>
+      <br />
+      <input type="text" placeholder="Linea 1" />
+      <br />
+      <input type="text" placeholder="Linea 2" />
+      <br />
+      <button>Exportar</button>
+      <div>
+        <span>{linea1}</span>
+        <span>{linea2}</span>
+        <img src={logo} alt="Logo" />
+      </div>
+      {/* Select Picker de memes */}
+      {/* Input text - Primer linea */}
+      {/* Input text - Segunda linea */}
+      {/* Boton exportar */}
     </div>
   );
 }
