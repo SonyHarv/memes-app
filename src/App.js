92a80d982b2ee36ee4553bf93a8e4c5a82a6e5f0
Erikson TF <sonyharv9@gmail.com>
2021-09-19 18:04:13 -0700

    add función evento.tagert.value en inputs y select

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [linea1, setLinea1] = useState("Hola Mundo");
-  const [linea2, setLinea2] = useState("Bienvenidos!!!");
+  const [linea1, setLinea1] = useState("");
+  const [linea2, setLinea2] = useState("");
+  const [imagen, setImagen] = useState("");
+
+  const onChangeLinea1 = function (evento) {
+    setLinea1(evento.target.value);
+  };
+  const onChangeLinea2 = function (evento) {
+    setLinea2(evento.target.value);
+  };
+  const onChangeImagen = function (evento) {
+    setImagen(evento.target.value);
+  };
 
   return (
     <div className="App">
-      <select>
+      <select onChange={onChangeImagen}>
         <option value="fire">Casa en llamas</option>
         <option value="futurama">Futurama</option>
         <option value="history">History</option>
@@ -17,15 +27,16 @@ function App() {
         <option value="smart">Smart Guy</option>
       </select>
       <br />
-      <input type="text" placeholder="Linea 1" />
+      <input type="text" placeholder="Linea 1" onChange={onChangeLinea1} />
       <br />
-      <input type="text" placeholder="Linea 2" />
+      <input type="text" placeholder="Linea 2" onChange={onChangeLinea2} />
       <br />
       <button>Exportar</button>
       <div>
         <span>{linea1}</span>
+        <br />
         <span>{linea2}</span>
-        <img src={logo} alt="Logo" />
+        <img src={"/assets/images/" + imagen + ".jpg"} alt="fire" />
       </div>
       {/* Select Picker de memes */}
       {/* Input text - Primer linea */}
